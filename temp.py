78613b19ce127fe9dfbe82b7dd9ea40bd47ef0c3
ACore3829 <andrewpc3829@gmail.com>
2023-04-15 13:07:49 -0600

    Update temp.py

--- temp.py
+++ temp.py
@@ -2,14 +2,18 @@ import random
 import time
 import sys
 import tkinter as tk
+from tkinter import *
+import threading
 
 root=tk.Tk()
 
-
+def run_main():
+    t = threading.Thread(target=main)
+    t.start()
 
 def main():
-    type_temp = input('Celsius or Farenheit')
-    temp_promtp = print('What is the temperature?')
+    type_temp = input('Celsius or Farenheit ')
+    temp_promtp = print('What is the temperature? ')
     temp = int(input())
     if type_temp == 'Celsius' or type_temp == 'C' or type_temp == 'c' or type_temp == 'celsius':
         temp_to_f_first = temp * 9/5
@@ -20,12 +24,11 @@ def main():
         temp_celsius = temp_to_c_first * 5/9
         print(temp_celsius)
 
-        
-
-button = tk.Button(root, text="Press Me", command=main)
+#size = Canvas(root, width=500, height=500)
+#size.pack()
+button = tk.Button(root, text="Press Me", command=run_main)
 button_end = tk.Button(root, text="Close", bg="red", fg="white", font="bold", width=5, height=1, command=root.destroy)
 button.pack()
 button_end.pack()
-
 
 root.mainloop()
